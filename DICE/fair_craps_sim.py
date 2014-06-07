83878e004e8be77440e42c1c62ea6ec6326924ed
Forced-Enthusiasm <t.m.dubiel@gmail.com>
2014-06-07 01:22:15 -0400

    simulates N craps games
    simulates N craps games using fair dice and tells you the probability you have to win on your first throw.

---
+++ DICE/fair_craps_sim.py
@@ -0,0 +1,74 @@
+import numpy as np
+
+#Generates a random sample of dice rolls on an even dice
+
+def fair_dice_sampler(N):
+
+	dice_values = [1,2,3,4,5,6]
+
+	number = float(1)/float(6)
+
+	probability_distribution = [number]*6
+
+	sampler = np.random.choice(dice_values,N, p=probability_distribution)
+
+	return sampler
+
+number_of_games = 6000
+
+sample = fair_dice_sampler(number_of_games)
+
+occurences = []
+
+number_of_occurences = 0
+
+for i in range(1,7):
+
+	for x in sample:
+		
+		if i == x:
+
+			number_of_occurences+=1
+
+	occurences.append(number_of_occurences)
+
+	number_of_occurences = 0	
+
+
+print "In " +str(number_of_games)+" rolls of a fair dice, we get:"
+
+print "1: " + str(occurences[0]) + " occurences"
+
+print "2: " + str(occurences[1]) + " occurences"
+
+print "3: " + str(occurences[2]) + " occurences"
+
+print "4: " + str(occurences[3]) + " occurences"
+
+print "5: " + str(occurences[4]) + " occurences"
+
+print "6: " + str(occurences[5]) + " occurences"
+
+
+# Simulating a Come-out roll for a craps game over 1000 games
+
+
+def fair_craps_simulator(N):
+
+	number_of_wins = 0
+
+	dice_1 = fair_dice_sampler(N)
+
+	dice_2 = fair_dice_sampler(N)
+
+	sum_of_dice = [sum(i) for i in zip(dice_1,dice_2)]
+
+	for x in sum_of_dice:
+
+		if x ==7 or x == 11:
+
+			number_of_wins += 1
+
+	return number_of_wins
+
+print "In 1000 games of craps with 2 fair dice, you should win on the first roll " + str(fair_craps_simulator(1000)) + " times."
